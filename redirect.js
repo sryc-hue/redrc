6465b2ceadba51ff1d0c833c590b30f64004ef3b
sryc-hue <64719718+sryc-hue@users.noreply.github.com>
2020-05-03 11:42:38 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,13 +1,1 @@
-  var url = "http://www.healthyfree.xyz/2020/02/6-warning-signs-that-indicate-your.html"; // url tujuan
-            var count = 1; // dalam detik
-            function countDown() {
-                if (count > 0) {
-                    count--;
-                    var waktu = count + 1;
-                    $('#kata').html('<b>Halaman Ini Akan Otomatis Di Redirect KE </b> ' + url + ' dalam ' + waktu + ' detik.');
-                    setTimeout("countDown()", 1000);
-                } else {
-                    window.location.href = url;
-                }
-            }
-            countDown();
+alert("Saya memanggil Javascript melalui alert.js");
