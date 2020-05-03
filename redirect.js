5a85152735cd3221d10441fc255365635f6a0ae3
sryc-hue <64719718+sryc-hue@users.noreply.github.com>
2020-05-03 11:30:28 +0700

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,13 @@
+ var url = "https://jagoankode.blogspot.com/"; // url tujuan
+            var count = 10; // dalam detik
+            function countDown() {
+                if (count > 0) {
+                    count--;
+                    var waktu = count + 1;
+                    $('#kata').html('<b>Halaman Ini Akan Otomatis Di Redirect KE </b> ' + url + ' dalam ' + waktu + ' detik.');
+                    setTimeout("countDown()", 1000);
+                } else {
+                    window.location.href = url;
+                }
+            }
+            countDown();
